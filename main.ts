7a408c67ae07f954cebb2160b14e1e1b34e8691a
cheshnut <mytomart2@gmail.com>
2025-12-14 15:46:24 +0700

    gitignore and client

--- main.ts
+++ main.ts
@@ -1,1 +1,9 @@
 import { createJupiterApiClient } from '@jup-ag/api';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const ENDPOINT = process.env.JUP_ENDPOINT;
+const CONFIG = {
+    basePath: ENDPOINT
+};
+const jupiterApi = createJupiterApiClient(CONFIG);
